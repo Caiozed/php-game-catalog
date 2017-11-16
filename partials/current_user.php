bb793c1bd50009b764c83667467c484be6a08a3c
Caio Neves <caio.nevesleite@gmail.com>
2017-11-16 22:15:20 +0000

    heroku update

--- partials/current_user.php
+++ partials/current_user.php
@@ -11,9 +11,10 @@
         $id = null;
     }
     
-    /*$query = "SELECT * FROM users WHERE id = '$id' ";
-    $result = mysqli_query($connection, $query);
-    $current_user = $result->fetch_array();
-    mysqli_close($connection);
-    echo*/
+    if($id){
+        $query = "SELECT * FROM users WHERE id = '$id' ";
+        $result = mysqli_query($connection, $query);
+        $current_user = $result->fetch_array();
+        mysqli_close($connection);
+    }
 ?>
